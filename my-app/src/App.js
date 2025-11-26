20fcee463d3a4629bf3cf631b8edeced4bac3596
Zach Hamby <zhamby1@ivytech.edu>
2025-11-26 09:32:27 -0500

    init

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,54 +1,38 @@
 //this is the new ES6 way to import modules...we say import and then the file location (. means current folder)
 import './App.css';
 
-
-// function Greeting(props) {
-//    function greetClick(event) {
-//       const greeting = event.target.id === "helloBtn" 
-//          ? "Hello" : "Hey";
-            
-//       alert(`${greeting}, ${props.name}!`);
-//    }
- 
-//    return (
-//       <div>
-//          <button id="helloBtn" onClick={greetClick}>
-//             Say Hello
-//          </button>
-//          <button id="heyBtn" onClick={greetClick}>
-//             Say Hey
-//          </button>
-//       </div>
-//    );
-// }
-function Greeting(props) {
-   function greetClick(greeting) {
-      alert(`${greeting}, ${props.name}!`);
+function Light(props) {
+   if (props.on) {
+      return <p>Light is on!</p>;
    }
- 
+   else {
+      return <p>Light is off.</p>;
+   }
+}
+
+
+function Account(props) {
+
+  if(props.balance > 0.00){
    return (
-      <div>
-         <button onClick={() => greetClick("Hello")}>
-            Say Hello
-         </button>
-         <button onClick={() => greetClick("Hey")}>
-            Say Hey
-         </button>
-      </div>
+      <>
+         <h1>Account Information</h1>
+         
+          <p>You owe ${props.balance}.</p>
+         
+      </>
+   );
+  }
+}
+
+
+function App() {
+   return (
+      <Account balance={10.00} />
    );
 }
 
-//the way react works is everything is surrounded by a function.  It must be a return function
-function App(){
-  //jsx - html and javascript mixed
-  //since we can mix html and JS, we can use js variables with html elements
-  //we are using const alot?  Why
-  //this all about state which we will talk about later but basically const is set but not changed and is re-rendered each time the page refreshes
-  return (
-    <Greeting name="Zach" />
-  )
-  
-}
+
 
 
 
