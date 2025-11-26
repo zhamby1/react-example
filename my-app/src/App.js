e41121f5c9a4f1f40368a01b2ba3d31c439e61ac
Zach Hamby <zhamby1@ivytech.edu>
2025-11-26 09:23:03 -0500

    init

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,41 +1,90 @@
-//this is the new ES6 way to import modules...we say import and then the file location (. means current folder)
-import './App.css';
+//we are going to import the useState module from react
+import { use, useState } from "react";
 
 
 // function Greeting(props) {
-//    function greetClick(event) {
-//       const greeting = event.target.id === "helloBtn" 
-//          ? "Hello" : "Hey";
-            
-//       alert(`${greeting}, ${props.name}!`);
-//    }
+//   //usestate allows us to creation a variable that lets us know that the state of the variable can change based on event or certain types of conditions.
+//   //all these variables will use an array that is almost like a getter and setter - data itself, and something to change the data
+//   //we can also give the data default values
+//   const [greeting, setGreeting] = useState("Hi")
+  
+   
  
 //    return (
-//       <div>
-//          <button id="helloBtn" onClick={greetClick}>
-//             Say Hello
-//          </button>
-//          <button id="heyBtn" onClick={greetClick}>
-//             Say Hey
-//          </button>
-//       </div>
+//     <div>
+//     <p>{greeting}</p>
+    
+//     <button onClick={() => setGreeting(`Hello, ${props.name}`)}>
+//       Say Hello
+//     </button>
+
+//     <button onClick={() => setGreeting(`Hey, ${props.name}`)}>
+//       Say Hey
+//     </button>
+
+//     </div>
+    
+
 //    );
 // }
-function Greeting(props) {
-   function greetClick(greeting) {
-      alert(`${greeting}, ${props.name}!`);
-   }
- 
-   return (
-      <div>
-         <button onClick={() => greetClick("Hello")}>
-            Say Hello
-         </button>
-         <button onClick={() => greetClick("Hey")}>
-            Say Hey
-         </button>
-      </div>
-   );
+
+function Movie(){
+  //multiple stateful variables
+  const [title,setTitle] = useState("Titanic")
+  const [rating,setRating] = useState("PG-13")
+  const [releaseYear, setReleaseYear] = useState("1996")
+  
+  function changeMovie(){
+    setTitle('Transformers')
+    setRating('PG')
+    setReleaseYear('2006')
+  }
+
+  return(
+    <>
+    <h1>My Favorite Movie</h1>
+    <p>
+      {title}, {rating}, {releaseYear}
+    </p>
+
+    <button onClick={changeMovie}>
+      Change My Favorite Movie
+    </button>
+    
+    </>
+  )
+}
+
+function MovieObject(){
+  //instead of having sepearted values, we instead want to create an object literal with a bunch of values inside of it
+
+  const [movie, setMovie] = useState({
+    title: 'Inception',
+    rating: 'PG-13',
+    releaseYear: '2010'
+  })
+  
+  function changeTitle(){
+    //many times when changing an object we don't need to change everything about an object
+    //we can use something called previous state to grab the current state and stick in just the changes
+    setMovie(prevState => ({
+      ...prevState, title:'Titanic'
+    }))
+  }
+
+  return(
+    <>
+    <h1>My Favorite Movie</h1>
+    <p>
+      {movie.title}, {movie.rating}, {movie.releaseYear}
+    </p>
+
+    <button onClick={changeTitle}>
+      Change The Title
+    </button>
+    
+    </>
+  )
 }
 
 //the way react works is everything is surrounded by a function.  It must be a return function
@@ -45,7 +94,7 @@ function App(){
   //we are using const alot?  Why
   //this all about state which we will talk about later but basically const is set but not changed and is re-rendered each time the page refreshes
   return (
-    <Greeting name="Zach" />
+    <MovieObject />
   )
   
 }
